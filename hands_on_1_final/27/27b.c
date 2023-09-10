d76936ea42d706030c390d67e1fa32e1b638e693
Rishav Chandel <82945614+Rgsis-commits@users.noreply.github.com>
2023-09-10 18:28:18 +0530

    Update 27b.c

--- hands_on_1_final/27/27b.c
+++ hands_on_1_final/27/27b.c
@@ -1,3 +1,11 @@
+/*
+============================================================================
+Name : 27b.c
+Author : Rishav Chandel
+Description : Write a program to execute ls -Rl by the following system calls
+                b. execlp
+============================================================================
+*/
 #include<stdio.h>
 #include<unistd.h>
 
