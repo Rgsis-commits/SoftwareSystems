976b6af5be6c13ccbc23d5f4696a17890183f0d8
Rishav Chandel <82945614+Rgsis-commits@users.noreply.github.com>
2023-09-10 18:12:38 +0530

    Update 14.c

--- hands_on_1_final/14/14.c
+++ hands_on_1_final/14/14.c
@@ -1,3 +1,12 @@
+/*
+============================================================================
+Name : 14.c
+Author : Rishav Chandel
+Description : Write a program to find the type of a file.
+ a. Input should be taken from command line.
+ b. program should be able to identify any type of a file. 
+============================================================================
+*/
 #include<stdio.h>
 #include<unistd.h>
 #include<fcntl.h>
