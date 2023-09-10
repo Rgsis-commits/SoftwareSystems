7de0973202466b62f8e3f1723a28ed6eaf00a3d8
Rishav Chandel <82945614+Rgsis-commits@users.noreply.github.com>
2023-09-10 18:06:43 +0530

    Update 8.c

--- hands_on_1_final/8/8.c
+++ hands_on_1_final/8/8.c
@@ -1,3 +1,11 @@
+/*
+============================================================================
+Name : 8.c
+Author : Rishav Chandel
+Description : Write a program to open a file in read only mode, read line by line and display each line as it is read.
+Close the file when end of file is reached.
+============================================================================
+*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
